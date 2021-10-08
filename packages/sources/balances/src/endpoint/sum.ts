aed801134dfa947cde3442498a37772900d82a03
32jvilla <32jvilla@gmail.com>
2021-10-08 15:56:30 -0700

    x

---
+++ packages/sources/balances/src/endpoint/sum.ts
@@ -0,0 +1,43 @@
+import { Requester, Validator } from '@chainlink/ea-bootstrap'
+import { Config, ExecuteWithConfig, InputParameters } from '@chainlink/types'
+import { NAME as AdapterName } from '../config'
+
+export const supportedEndpoints = ['sum']
+
+export const endpointResultPaths = {
+  example: 'price',
+}
+
+export interface ResponseSchema {
+  data: {
+    sum: number
+  }
+}
+
+const customError = (data: any) => data.Response === 'Error'
+
+export const inputParameters: InputParameters = {}
+
+export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
+  const validator = new Validator(request, inputParameters)
+  if (validator.error) throw validator.error
+
+  const jobRunID = validator.validated.id
+  const base = validator.overrideSymbol(AdapterName)
+  const quote = validator.validated.data.quote
+  const url = `price`
+  const resultPath = validator.validated.data.resultPath
+
+  const params = {
+    base,
+    quote,
+    api_key: config.apiKey,
+  }
+
+  const options = { ...config.api, params, url }
+
+  const response = await Requester.request<ResponseSchema>(options, customError)
+  const result = Requester.validateResultNumber(response.data, [resultPath])
+
+  return Requester.success(jobRunID, Requester.withResult(response, result), config.verbose)
+}
