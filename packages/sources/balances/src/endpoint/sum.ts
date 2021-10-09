8e5304ac805de88707d6e7f9d5ad438a68e29233
32jvilla <61704946+32jvilla@users.noreply.github.com>
2021-10-09 13:38:33 -0700

    Working Sum Value Returned

--- packages/sources/balances/src/endpoint/sum.ts
+++ packages/sources/balances/src/endpoint/sum.ts
@@ -1,6 +1,5 @@
 import { Requester, Validator } from '@chainlink/ea-bootstrap'
 import { Config, ExecuteWithConfig, InputParameters } from '@chainlink/types'
-// import { NAME as AdapterName } from '../config'
 
 export const supportedEndpoints = ['sum']
 
@@ -8,36 +7,21 @@ export const endpointResultPaths = {
   example: 'price',
 }
 
-export interface ResponseSchema {
-  data: Array<{
-    address: string
-    balance: number
-  }>
-  result: number
-}
+type ResponseSchema = Array<{ address: string; balance: number }>
 
 export const inputParameters: InputParameters = {}
 
 export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
   const validator = new Validator(request, inputParameters)
   if (validator.error) throw validator.error
-  // const resultPath = validator.validated.data.resultPath
-
   const jobRunID = validator.validated.id
-  const response = await Requester.request(config.api)
 
+  const response = await Requester.request<ResponseSchema>(config.api)
   const result = response.data
-    .map((wallet: { address: string; balance: number }) => wallet.balance)
-    .reduce(function (a: number, b: number) {
+    .map((wallet) => wallet.balance)
+    .reduce(function (a, b) {
       return a + b
     })
-  console.log(result)
 
-  // response.data = Requester.validateResultNumber(response.data, [
-  //   'address',
-  //   'balance',
-  // ])
-
-  // return Requester.success(jobRunID, Requester.withResult(response, result), config.verbose)
-  return Requester.success(jobRunID, response, config.verbose)
+  return Requester.success(jobRunID, Requester.withResult(response, result), config.verbose)
 }
